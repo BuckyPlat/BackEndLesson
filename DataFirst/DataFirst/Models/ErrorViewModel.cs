ef8fac7dd31d08463ed91a5f33987dfa074f7474
BuckyPlat <vonhanhongphuc@gmail.com>
2025-11-12 22:52:04 +0700

    DataFirst

---
+++ DataFirst/DataFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
