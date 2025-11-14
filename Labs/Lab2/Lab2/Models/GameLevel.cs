653ffff0e881507172ce9e286cc90165bca42af2
BuckyPlat <vonhanhongphuc@gmail.com>
2025-11-14 11:53:06 +0700

    Lab2 completed

---
+++ Labs/Lab2/Lab2/Models/GameLevel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Lab2.Models
+{
+    public class GameLevel
+    {
+        [Key]
+        public int LevelId { get; set; }
+        public string title { get; set; }
+        public string? description { get; set; }
+    }
+}
